55966f6dbf49f9167e792eb73becab89a0ce4633
Ryan Watkins <17993027+r-watkins@users.noreply.github.com>
2025-03-20 15:20:55 -0400

    update page transition

--- src/components/Curtain/styles.js
+++ src/components/Curtain/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   position: absolute;
-  top: 0;
+  top: 70px;
   left: 50%;
   z-index: ${(props) => props.theme.levels.top};
   display: flex;
@@ -15,7 +15,28 @@ const Container = styled.div`
 const Cover = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: ${(props) => props.theme.palette.background};
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 51%;
+    background-color: ${(props) => props.theme.palette.primary};
+    transform: skew(0deg, 6deg);
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 50%;
+    background-color: ${(props) => props.theme.palette.primary};
+    transform: skew(0deg, -6deg);
+  }
 `;
 
 export { Container, Cover };
